d56e4a60618ff19932b5f1350a8ff93d50b2fbe1
Alex Eapen <ak.eapen@gmail.com>
2024-01-06 11:28:13 -0800

    add week4

---
+++ week4/test_debug_codelldb/src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+// this example is useful application of `while` because it allows to continue 
+// asking for input until the user types a specific word (in this case "stop")
+
+fn main() {
+    let mut input = String::new();
+    while input.trim() != "stop" {
+        println!("Type something ('stop' to quit):");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input line");
+        println!("You typed: {}", input);
+    }
+    println!("Goodbye!");
+}
---
+++ week4/test_my_library/src/main.rs
@@ -0,0 +1,7 @@
+use my_library::hello_world;
+use my_library::add;
+
+fn main() {
+    hello_world();
+    println!("202 + 132 = {}", add(202, 132));
+}
